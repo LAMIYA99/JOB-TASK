4d248ab82e188475295275ea9b846fb2300ed990
LAMIYA ALIZADA <lamiyaea@code.edu.az>
2025-11-04 20:38:54 +0400

    navbar added

--- client/src/Layout/Header/index.tsx
+++ client/src/Layout/Header/index.tsx
@@ -3,7 +3,7 @@ import { BriefcaseBusiness, ChevronDown, Lock } from "lucide-react"
 const Header = () => {
   return (
 <header className="sticky top-0 bg-white z-[999] container mx-auto max-w-[1200px] py-[13px]">
-  <nav className="grid grid-cols-12  flex justify-between ">
+  <nav className="grid grid-cols-12 items-center   justify-between ">
     <div className="col-span-3">
       <div className="flex items-center">
         <img
@@ -14,7 +14,7 @@ const Header = () => {
       </div>
     </div>
 
-    <div className="col-span-9 flex items-center gap-8">
+    <div className="col-span-9 flex items-center justify-end gap-8">
       <ul className="flex items-center gap-6 text-[#232535] text-[14px] font-medium leading-5 font-inter">
         <li className="cursor-pointer">Home</li>
         <li className="cursor-pointer">About</li>
@@ -24,11 +24,11 @@ const Header = () => {
         <li className="cursor-pointer">Cart (0)</li>
       </ul>
 
-      <div className="flex items-center justify-center gap-4">
-        <button className="flex items-center gap-2 px-1.5 py-1.5 border border-[#E2E8F0] rounded-[8px] shadow-2xs bg-[#F8F8FA]">
+      <div className="flex items-end justify-end gap-4">
+        <button className="flex items-center gap-2 p-2 border border-[#E2E8F0] text-14px font-medium rounded-[8px] shadow-2xs bg-[#F8F8FA]">
           <Lock size={14} /> Login
         </button>
-        <button className="flex items-center gap-2 px-1.5 py-1.5 bg-[#4767FF] border border-[#4767FF] rounded-[8px] shadow-2xs text-white">
+        <button className="flex items-center gap-2  p-2 text-[14px] font-medium bg-[#4767FF] border border-[#4767FF] rounded-[8px] shadow-2xs text-white">
           <BriefcaseBusiness size={14} /> Post a job
         </button>
       </div>
